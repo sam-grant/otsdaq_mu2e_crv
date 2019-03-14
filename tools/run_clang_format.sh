d81558b1061112594f19cb82535a664b05a06ca4
Ryan Rivera <rrivera@fnal.gov>
2019-03-14 09:25:34 -0500

    cleaning up detector tools directories

--- tools/run_clang_format.sh
+++
@@ -1,7 +0,0 @@
-
-if [ ! -d ${CLANG_FQ_DIR:-""} ]; then
-    echo "You must first setup Clang! (setup clang v5_0_1)"
-    exit 1
-fi
-clang-format -i `find . -type f -name *.icc -o -name *.cc -o -name *.c -o -name *.cpp -o -name *.cxx -o -name *.h -o -name *.hh -o -name *.hxx -o -name *.hpp`
-
