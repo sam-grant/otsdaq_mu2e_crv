6fab83d966518b1ea98cba76923eb5a33c3a95ca
Mu2E <mu2edaq@mu2edaq01.fnal.gov>
2019-04-30 11:32:49 -0500

    read block functional through FE Macros

--- otsdaq-mu2e-crv/FEInterfaces/ROCCosmicRayVetoInterface.h
+++ otsdaq-mu2e-crv/FEInterfaces/ROCCosmicRayVetoInterface.h
@@ -35,6 +35,10 @@ public:
                                      unsigned data_to_write) override;
   virtual int readEmulatorRegister(unsigned address) override;
 
+  virtual void readROCBlock(std::vector<uint16_t>& data, unsigned address,unsigned numberOfReads,unsigned incrementAddress) override{  }
+  virtual void readEmulatorBlock(std::vector<uint16_t>& data, unsigned address,unsigned numberOfReads,unsigned incrementAddress) override {  }
+
+
   // specific ROC functions
   virtual int readTimestamp() override;
   virtual void writeDelay(unsigned delay) override; // 5ns steps
